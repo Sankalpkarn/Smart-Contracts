feadae50c4aa82df92ce85e265a229bca07be579
Sankalp karn <121498981+Sankalpkarn@users.noreply.github.com>
2025-12-26 12:26:48 +0530

    Update ToDo.sol

--- ToDo.sol
+++ ToDo.sol
@@ -17,12 +17,21 @@ contract ToDoList {
         tasks.push(task("Walking", false));
     }
 
-    function taskupdate(uint taskId) public {
+    function addtasks(string calldata _taskname) external {
+        tasks.push(task(_taskname, false));
+
+    }
+
+    function statusupdate(uint taskId) public {
             tasks[taskId].taskstatus = true;
             emit taskmarked(taskId);
+    }
+
+    function getList() public view returns (task[] memory) {
+        return tasks;
     }
 
 
 
 }
-// This code is To-DO List, here a user can check whether task is done or not and can mark it as true if updated 
+// This code is To-DO List, here a user can check whether task is done or not and can mark it as true if updated, you can add a task
